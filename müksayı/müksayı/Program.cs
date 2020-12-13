14a31392ad35b13ad1defb914be8c28e69bf6ad0
DarklyPixel <75914160+DarklyPixel@users.noreply.github.com>
2020-12-13 13:13:22 +0300

    Add files via upload

---
+++ müksayı/müksayı/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace müksayı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi;
+           
+            int toplam = 0;
+            for (int i = 1; i <= sayi / 2; i++)
+            {
+
+                if (sayi % i == 0)
+                {
+                    toplam = toplam + i;
+                }
+            }
+            if (sayi == toplam)
+            {
+              Console.Write("Girdiğiniz Sayı Bir Mükemmel Sayıdır");
+            }
+            else
+            {
+               Console.Write("Girdiğiniz Sayı Bir Mükemmel Sayı Değildir");
+            }
+
+        }
+
+    }
+    }
+
